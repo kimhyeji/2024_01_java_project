5bbbfd552cf5a58fd85805f3211b3a6d4cdc0f10
kimhyeji <gpwl5589@gmail.com>
2024-02-19 15:16:43 +0900

    게시물 작성 기능 구현 중

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
-
         Scanner sc = new Scanner(System.in);
-        System.out.printf("명령어) ");
-        String cmd = sc.nextLine();
-        System.out.printf("입력된 명령어 : %s\n", cmd);
-        
-        System.out.println("소스코드 수정");
+
+        int lastArticleId = 0;
+
+        while ( true ) {
+            System.out.printf("명령어) ");
+            String cmd = sc.nextLine();
+
+            cmd = cmd.trim();
+
+            if ( cmd.length() == 0 ) {
+                continue;
+            }
+
+            if ( cmd.equals("system exit") ) {
+                break;
+            }
+            else if ( cmd.equals("article list") ) {
+                System.out.println("게시물이 없습니다.");
+            }
+            else if ( cmd.equals("article write") ) {
+                int id = lastArticleId + 1;
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+
+                lastArticleId = id;
+
+                System.out.printf("%d번 글이 작성되었습니다.\n", id);
+            }
+            else {
+                System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
+            }
+        }
 
         sc.close();
 
