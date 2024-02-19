6ea657f30aafdf6b79ba5047523e6cd568ef6647
kimhyeji <gpwl5589@gmail.com>
2024-02-19 15:37:52 +0900

    게시물 저장 힌트

---
+++ src/main/java/org/example/Exam.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inputNum = -1;
+
+        List<Integer> nums = new ArrayList<>();
+
+        while ( true ) {
+            System.out.printf("명령어) ");
+            String cmd = sc.nextLine();
+            cmd = cmd.trim();
+
+            if ( cmd.length() == 0 ) {
+                continue;
+            }
+
+            if ( cmd.equals("exit") ) {
+                break;
+            }
+
+            else if ( cmd.equals("num") ) {
+                System.out.printf("숫자 : ");
+                int num = sc.nextInt();
+                sc.nextLine();
+
+                nums.add(num);
+
+                inputNum = num;
+
+            }
+            else if ( cmd.equals("nums") ) {
+                System.out.printf("숫자들 : %s\n", nums.toString());
+            }
+        }
+
+
+
+        sc.close();
+    }
+}
