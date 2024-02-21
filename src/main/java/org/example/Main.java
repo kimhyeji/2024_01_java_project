efb4b4269e797b59e156da804669735169925ec9
kimhyeji <gpwl5589@gmail.com>
2024-02-21 15:24:32 +0900

    게시물 작성 시 작성날짜도 저장

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -61,7 +61,7 @@ public class Main {
                 }
 
                 System.out.printf("번호 : %d\n", foundArticle.id);
-                System.out.printf("날짜 : %s\n", "2020-12-12 12:12:12");
+                System.out.printf("날짜 : %s\n", foundArticle.regDate);
                 System.out.printf("제목 : %s\n", foundArticle.title);
                 System.out.printf("내용 : %s\n", foundArticle.body);
             }
@@ -91,13 +91,13 @@ public class Main {
             }
             else if ( cmd.equals("article write") ) {
                 int id = lastArticleId + 1;
+                lastArticleId = id;
+                String regDate = Util.getNowDateStr();
                 System.out.printf("제목 : ");
                 String title = sc.nextLine();
                 System.out.printf("내용 : ");
                 String body = sc.nextLine();
-
-                lastArticleId = id;
-                Article article = new Article(id, title, body);
+                Article article = new Article(id, regDate, title, body);
 
                 articles.add(article);
 
@@ -116,11 +116,13 @@ public class Main {
 
 class Article {
     int id;
+    String regDate;
     String title;
     String body;
 
-    public Article (int id, String title, String body) {
+    public Article (int id, String regDate, String title, String body) {
         this.id = id;
+        this.regDate = regDate;
         this.title = title;
         this.body = body;
     }
